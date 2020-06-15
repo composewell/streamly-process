b4da924118f881d5c5864107a35c367f8123d49f
Ahmed Zaheer Dadarkar <ahmed.dadarkar@composewell.com>
2020-06-15 17:38:00 +0530

    Add Benchmarks
    Update benchmark code

--- Benchmark/System/Process.hs
+++ Benchmark/System/Process.hs
@@ -16,8 +16,14 @@ devNull = "/dev/null"
 catBinary :: String
 catBinary = "/bin/cat"
 
+trBinary :: String
+trBinary = "/usr/bin/tr"
+
 largeFile :: String
 largeFile = "/Users/ahmed/Downloads/largeFile"
+
+smallFile :: String
+smallFile = "/Users/ahmed/Downloads/smallFile"
 
 runCatCmd :: IO ()
 runCatCmd = do
@@ -31,8 +37,26 @@ runCatCmdChunk = do
     FH.fromChunks hdl $ Proc.fromExeChunks catBinary [largeFile]
     hClose hdl
 
+runTrCmd :: IO ()
+runTrCmd = do
+    inputHdl <- openFile largeFile ReadMode
+    outputHdl <- openFile devNull WriteMode
+    FH.fromBytes outputHdl $ Proc.thruExe_ trBinary ["[a-z]", "[A-Z]"] (FH.toBytes inputHdl)
+    hClose inputHdl
+    hClose outputHdl
+
+runTrCmdChunk :: IO ()
+runTrCmdChunk = do
+    inputHdl <- openFile largeFile ReadMode
+    outputHdl <- openFile devNull WriteMode
+    FH.fromChunks outputHdl $ Proc.thruExeChunks_ trBinary ["[a-z]", "[A-Z]"] (FH.toChunks inputHdl)
+    hClose inputHdl
+    hClose outputHdl
+
 main :: IO ()
 main = defaultMain [
         bench "exe - word8" $ nfIO runCatCmd,
-        bench "exe - array of word8" $ nfIO runCatCmdChunk
+        bench "exe - array of word8" $ nfIO runCatCmdChunk,
+        bench "exe - word8 to word8" $ nfIO runTrCmd,
+        bench "exe - array of word8 to array of word8" $ nfIO runTrCmdChunk
     ]
