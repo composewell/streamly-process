3879907710be32a5797082bbd072d25a47adb5a4
Ahmed Zaheer Dadarkar <ahmed.dadarkar@composewell.com>
2020-06-28 19:55:19 +0530

    Update test to remove IORef

--- Benchmark/System/Process.hs
+++ Benchmark/System/Process.hs
@@ -4,7 +4,7 @@ import qualified Streamly.Internal.Prelude as S
 import qualified Streamly.System.Process as Proc
 import qualified Streamly.Internal.FileSystem.Handle as FH
 
-import System.IO (FilePath, Handle, IOMode(..), openFile, hClose)
+import System.IO (FilePath, Handle, IOMode(..), openFile, hClose, stdout)
 import System.Process (proc, createProcess, waitForProcess)
 import System.Directory (removeFile, findExecutable)
 
@@ -14,7 +14,7 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.IORef (IORef (..), newIORef, readIORef, writeIORef)
 import Data.Word (Word8)
 
-import Gauge (Benchmarkable, defaultMain, bench, nfIO, perRunEnvWithCleanup)
+import Gauge (Benchmarkable, defaultMain, bench, nfIO, perRunEnv, perRunEnvWithCleanup)
 
 _a :: Word8
 _a = 97
@@ -89,69 +89,81 @@ deleteFiles :: IO ()
 deleteFiles = 
     removeFile largeByteFile >> removeFile largeCharFile
 
-runCatCmd :: IORef Handle -> IO ()
-runCatCmd ioRefHandle = do
-    hdl <- readIORef ioRefHandle
+runCatCmd :: Handle -> IO ()
+runCatCmd hdl = do
     catBinary <- ioCatBinary
     FH.fromBytes hdl $ Proc.toBytes catBinary [largeByteFile]
 
-runCatCmdChunk :: IORef Handle -> IO ()
-runCatCmdChunk ioRefHandle = do
-    hdl <- readIORef ioRefHandle
+runCatCmdChunk :: Handle -> IO ()
+runCatCmdChunk hdl = do
     catBinary <- ioCatBinary
     FH.fromChunks hdl $ 
         Proc.toChunks catBinary [largeByteFile]
 
-runTrCmd :: IORef (Handle, Handle) -> IO ()
-runTrCmd ioRefHandles = do
-    (inputHdl, outputHdl) <- readIORef ioRefHandles
+runTrCmd :: (Handle, Handle) -> IO ()
+runTrCmd (inputHdl, outputHdl) = do
     trBinary <- ioTrBinary
     FH.fromBytes outputHdl $ 
         Proc.transformBytes trBinary ["[a-z]", "[A-Z]"] (FH.toBytes inputHdl)
 
-runTrCmdChunk :: IORef (Handle, Handle) -> IO ()
-runTrCmdChunk ioRefHandles = do
-    (inputHdl, outputHdl) <- readIORef ioRefHandles
+runTrCmdChunk :: (Handle, Handle) -> IO ()
+runTrCmdChunk (inputHdl, outputHdl) = do
     trBinary <- ioTrBinary
     FH.fromChunks outputHdl $ 
         Proc.transformChunks trBinary ["[a-z]", "[A-Z]"] (FH.toChunks inputHdl)
 
-benchWithOut :: (IORef Handle -> IO ()) -> Benchmarkable
-benchWithOut func = perRunEnvWithCleanup envIO closeHandle func
+benchWithOut :: IORef Handle -> (Handle -> IO ()) -> Benchmarkable
+benchWithOut nullFileIoRef func = perRunEnv openNewHandle benchCode
 
-    where 
+    where
     
-    envIO = do
-        outputHdl <- openFile devNull WriteMode
-        newIORef outputHdl
+    openNewHandle = do
+        oldHandle <- readIORef nullFileIoRef
+        hClose oldHandle
+        newHandle <- openFile devNull WriteMode
+        writeIORef nullFileIoRef newHandle
 
-    closeHandle ioRefOutputHdl = do
-        outputHdl <- readIORef ioRefOutputHdl
-        hClose outputHdl
+    benchCode _ = do
+        handle <- readIORef nullFileIoRef
+        func handle
 
-benchWithInpOut :: (IORef (Handle, Handle) -> IO ()) -> Benchmarkable
-benchWithInpOut func = perRunEnvWithCleanup envIO closeHandles func
+benchWithInpOut :: IORef (Handle, Handle) -> ((Handle, Handle) -> IO ()) -> Benchmarkable
+benchWithInpOut inpOutIoRef func = perRunEnv openNewHandles benchCode
 
-    where 
+    where
     
-    envIO = do 
-        inputHdl <- openFile largeCharFile ReadMode
-        outputHdl <- openFile devNull WriteMode
-        newIORef (inputHdl, outputHdl)
+    openNewHandles = do
+        (oldInputHdl, oldOutputHdl) <- readIORef inpOutIoRef
+        hClose oldInputHdl
+        hClose oldOutputHdl
+        newInputHdl <- openFile largeCharFile ReadMode
+        newOutputHdl <- openFile devNull WriteMode
+        writeIORef inpOutIoRef (newInputHdl, newOutputHdl)
 
-    closeHandles ioRefHandles = do
-        (inputHdl, outputHdl) <- readIORef ioRefHandles
-        hClose inputHdl
-        hClose outputHdl
+    benchCode _ = do
+        inpOutHdls <- readIORef inpOutIoRef
+        func inpOutHdls
 
 main :: IO ()
 main = do
     generateFiles
+    tempHandleWrite <- openFile devNull WriteMode
+    tempHandleRead <- openFile devNull ReadMode
+    ioRefOut <- newIORef tempHandleWrite
+    ioRefInpOut <- newIORef (tempHandleRead, tempHandleWrite)
     defaultMain [
-            bench "exe - word8" $ benchWithOut runCatCmd,
-            bench "exe - array of word8" $ benchWithOut runCatCmdChunk,
-            bench "exe - word8 to word8" $ benchWithInpOut runTrCmd,
+            bench "exe - word8" $ 
+                benchWithOut ioRefOut runCatCmd,
+            bench "exe - array of word8" $
+                benchWithOut ioRefOut runCatCmdChunk,
+            bench "exe - word8 to word8" $ 
+                benchWithInpOut ioRefInpOut runTrCmd,
             bench "exe - array of word8 to array of word8" $
-                benchWithInpOut runTrCmdChunk
+                benchWithInpOut ioRefInpOut runTrCmdChunk
         ]
+    handleOut1 <- readIORef ioRefOut
+    hClose handleOut1
+    (handleIn2, handleOut2) <- readIORef ioRefInpOut
+    hClose handleIn2
+    hClose handleOut2
     deleteFiles
