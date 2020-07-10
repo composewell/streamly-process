3c934727b515267ee0e7469396a9d8ac375047a4
Ahmed Zaheer Dadarkar <ahmed.dadarkar@composewell.com>
2020-07-10 17:55:33 +0530

    Add inline and optimizations

--- src/Streamly/System/Utility.hs
+++ src/Streamly/System/Utility.hs
@@ -15,21 +15,26 @@ import Control.Applicative ((<|>))
 import Control.Monad.Catch (MonadCatch)
 import Control.Exception (displayException)
 
+{-# INLINE isEofD #-}
 isEofD :: Monad m => D.Parser m a Bool
 isEofD = D.Parser step initial return
   where
     initial = return True
     step _ _ = return $ Done 1 False
 
+{-# INLINE isEof #-}
 isEof :: MonadCatch m => Parser m a Bool
 isEof = D.toParserK isEofD
 
+{-# INLINE conv2list #-}
 conv2list :: Monad m => Parser m a b -> Parser m a [b]
 conv2list parser = fmap (\x -> [x]) parser
 
+{-# INLINE singleQuote #-}
 singleQuote :: MonadCatch m => Parser m Char [Char]
 singleQuote = conv2list $ P.satisfy (=='\'')
 
+{-# INLINE parseWithinSingle #-}
 parseWithinSingle :: MonadCatch m => Parser m Char String
 parseWithinSingle = do
     let failCond char = char /= '\\' && char /= '\''
@@ -43,18 +48,22 @@ parseWithinSingle = do
             restStr <- parseWithinSingle
             return (str ++ [escapedChar] ++ restStr)
 
+{-# INLINE parseSingleQuoteArg #-}
 parseSingleQuoteArg :: MonadCatch m => Parser m Char String
 parseSingleQuoteArg =
     concatThreeList <$> singleQuote <*> parseWithinSingle <*> singleQuote
 
     where concatThreeList xs ys zs = xs ++ ys ++ zs
 
+{-# INLINE parseManySingleQuote #-}
 parseManySingleQuote :: MonadCatch m => Parser m Char String
 parseManySingleQuote = P.some FL.mconcat parseSingleQuoteArg
 
+{-# INLINE doubleQuote #-}
 doubleQuote :: MonadCatch m => Parser m Char [Char]
 doubleQuote = conv2list $ P.satisfy (=='"')
 
+{-# INLINE parseWithinDouble #-}
 parseWithinDouble :: MonadCatch m => Parser m Char String
 parseWithinDouble = do
     let noFailCond char = char /= '\\' && char /= '"'
@@ -68,15 +77,18 @@ parseWithinDouble = do
             restStr <- parseWithinDouble
             return (str ++ [escapedChar] ++ restStr)
 
+{-# INLINE parseDoubleQuoteArg #-}
 parseDoubleQuoteArg :: MonadCatch m => Parser m Char String
 parseDoubleQuoteArg =
     concatThreeList <$> doubleQuote <*> parseWithinDouble <*> doubleQuote
 
     where concatThreeList xs ys zs = xs ++ ys ++ zs
 
+{-# INLINE parseManyDoubleQuote #-}
 parseManyDoubleQuote :: MonadCatch m => Parser m Char String
 parseManyDoubleQuote = P.some FL.mconcat parseDoubleQuoteArg
 
+{-# INLINE parseNoQuote #-}
 parseNoQuote :: MonadCatch m => Parser m Char String
 parseNoQuote = do
     let noFailCond char = char /= '\\' && char /= '"' && char /= ' '
@@ -92,9 +104,11 @@ parseNoQuote = do
             else
                 P.die "Command Parse Error"
 
+{-# INLINE parseArgument #-}
 parseArgument :: MonadCatch m => Parser m Char String
 parseArgument = parseManySingleQuote <|> parseManyDoubleQuote <|> parseNoQuote
 
+{-# INLINE parseCommand #-}
 parseCommand :: MonadCatch m => Parser m Char (String, [String])
 parseCommand = do
     let 
